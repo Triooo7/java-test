38bd72a263839d4505ee10e06b82146727f7e07d
rohithraju <krishna.raju93@gmail.com>
2017-10-25 10:40:27 +0000

    initial java commit

---
+++ source/Rectangluator.java
@@ -0,0 +1,75 @@
+
+
+	public class Rectangluator {
+	public static void main (String[] args) {
+
+		int length = Integer.parseInt(args[0]);
+		int width = Integer.parseInt(args[1]);
+
+
+		Rectangle myRectangle = new Rectangle (length, width);
+
+		String output = String.format("********** \n L: %d\n W: %d \n A:		%d \n P: , myRectangle.length, myRectangle.width, myRectangle.getArea(), myRectangle.getPerimeter());
+	System.out.println(output);
+}
+
+
+
+
+
+
+
+
+
+
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+}
